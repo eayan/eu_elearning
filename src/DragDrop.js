5031cf588e22c7c37831e843952c9e935a69d7fa
eayan <erdal_ayan@yahoo.com>
2024-07-19 12:02:10 +0200

    drag-drop was updated

--- src/DragDrop.js
+++ src/DragDrop.js
@@ -1,9 +1,9 @@
 // src/DragDrop.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import './DragDrop.css';
 
 const DragDrop = () => {
-  const initialWords = [
+  const initialWords = useMemo(() => [
     { id: 1, word: 'dolor', correct: false },
     { id: 8, word: 'delectus', correct: false },
     { id: 3, word: 'voluptatem', correct: false },
@@ -12,7 +12,7 @@ const DragDrop = () => {
     { id: 6, word: 'doloribus', correct: false },
     { id: 7, word: 'delectus', correct: false },
     { id: 2, word: 'sunt', correct: false },
-  ];
+  ], []);
 
   const targetSentence = 'Lorem ipsum {0} sit amet. Sit fugit {1} est debitis dicta ex {2} dolorum qui cumque eveniet sed sint nesciunt sit voluptatem optio aut vitae sint. Ex {3} alias non impedit galisum non labore exercitationem vel accusantium aliquam. Aut unde sunt qui {4} doloribus sit internos temporibus et dolores error qui sapiente odit. Vel libero quod nam omnis aliquid aut harum neque. Ut {5} modi aut velit accusantium aut temporibus quae sed facilis dolor nam quas {6} et numquam sint. Ut veritatis aspernatur sed magnam fugiat qui tempore autem aut quae fugit a ratione iusto! Sed delectus autem aut quam sapiente et cumque eveniet! Aut mollitia consequatur et officia delectus et nihil modi quo quod quae sed laboriosam {7}!';
 
@@ -21,14 +21,14 @@ const DragDrop = () => {
   const [blanks, setBlanks] = useState(Array(initialWords.length).fill(null));
   const [checked, setChecked] = useState(false);
 
-  useEffect(() => {
-    shuffleWords();
-  }, []);
-
-  const shuffleWords = () => {
+  const shuffleWords = useCallback(() => {
     const shuffledWords = [...initialWords].sort(() => Math.random() - 0.5);
     setWords(shuffledWords);
-  };
+  }, [initialWords]);
+
+  useEffect(() => {
+    shuffleWords();
+  }, [shuffleWords]);
 
   const handleDragStart = (word) => {
     setDraggedWord(word);
